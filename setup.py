29e3acbceb4ddb1fdd47bb705219fb847e5c8c18
Jos Craw <jos@joscraw.net>
2020-04-30 15:42:54 +1200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", 'r') as file_handler:
+    long_description  = file_handler.read()
+
+setuptools.setup(
+    name="comms-utils",
+    version="0.0.1",
+    author="Jos Craw",
+    author_email="Jos Craw",
+    description="A set of tools for the ENEL422 Comms Assignment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.sys-io.net/scm/enel422/comms-utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
