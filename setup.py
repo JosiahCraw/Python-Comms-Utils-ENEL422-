d9f3be4adcfed198ee0d331f956b7341796c0797
Jos Craw <jos@joscraw.net>
2020-10-03 21:25:14 +1300

    Moved to v1.0.0

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", 'r') as file_handler:
 
 setuptools.setup(
     name="comms-utils",
-    version="0.0.2",
+    version="1.0.0",
     author="Jos Craw",
     author_email="jos@joscraw.net",
     description="A set of tools for the ENEL422 Comms Assignment",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git.sys-io.net/scm/enel422/comms-utils",
+    url="https://github.com/JosiahCraw/Python-Comms-Utils-ENEL422-",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
